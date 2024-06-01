33bd21b07f772a0f2724adc149a2eab30ee0df88
Jessica Noya <jessicapnoya@gmail.com>
2024-06-01 19:08:08 -0400

    Removed unnecessary comments and added event listener to navFavorites

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,5 @@
 "use strict";
 
-/******************************************************************************
- * Handling navbar clicks and updating navbar
- */
-
-/** Show main list of all stories when click site name */
-
 function navAllStories(evt) {
   console.debug("navAllStories", evt);
   hidePageComponents();
@@ -13,8 +7,6 @@ function navAllStories(evt) {
 }
 
 $body.on("click", "#nav-all", navAllStories);
-
-/** Show login/signup on click on "login" */
 
 function navLoginClick(evt) {
   console.debug("navLoginClick", evt);
@@ -24,8 +16,6 @@ function navLoginClick(evt) {
 }
 
 $navLogin.on("click", navLoginClick);
-
-/** When a user first logins in, update the navbar to reflect that. */
 
 function updateNavOnLogin() {
   console.debug("updateNavOnLogin");
@@ -43,3 +33,9 @@ function navSubmitStoryClick(evt) {
 }
 
 $navSubmitStory.on("click", navSubmitStoryClick);
+
+$navFavorites.on("click", function (evt) {
+  console.debug("favorites link clicked", evt);
+  hidePageComponents();
+  putFavoritesListOnPage();
+});
