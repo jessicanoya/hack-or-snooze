8a140194e811e8ebe0b4a4c19b98b57e8f53d41d
Jessica Noya <jessicapnoya@gmail.com>
2024-06-01 19:13:49 -0400

    Removed comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// So we don't have to keep re-finding things on page, find DOM elements once:
-
 const $body = $("body");
 
 const $storiesLoadingMsg = $("#stories-loading-msg");
@@ -17,35 +15,27 @@ const $navLogOut = $("#nav-logout");
 const $submitForm = $("#submit-form");
 const $navSubmitStory = $("#nav-submit-story");
 
-/** To make it easier for individual components to show just themselves, this
- * is a useful function that hides pretty much everything on the page. After
- * calling this, individual components can re-show just what they want.
- */
+const $favoritedStories = $("#favorited-stories");
+const $navFavorites = $("#nav-favorites");
 
 function hidePageComponents() {
-  const components = [$allStoriesList, $loginForm, $signupForm, $submitForm];
+  const components = [
+    $allStoriesList,
+    $loginForm,
+    $signupForm,
+    $submitForm,
+    $favoritedStories,
+  ];
   components.forEach((c) => c.hide());
 }
-
-/** Overall function to kick off the app. */
 
 async function start() {
   console.debug("start");
 
-  // "Remember logged-in user" and log in, if credentials in localStorage
   await checkForRememberedUser();
   await getAndShowStoriesOnStart();
 
-  // if we got a logged-in user
   if (currentUser) updateUIOnUserLogin();
 }
 
-// Once the DOM is entirely loaded, begin the app
-
-console.warn(
-  "HEY STUDENT: This program sends many debug messages to" +
-    " the console. If you don't see the message 'start' below this, you're not" +
-    " seeing those helpful debug messages. In your browser console, click on" +
-    " menu 'Default Levels' and add Verbose"
-);
 $(start);
